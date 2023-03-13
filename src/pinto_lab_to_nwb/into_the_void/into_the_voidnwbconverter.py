fe600e672fa2dba10dfef10f976cce1585e7e4ab
weiglszonja <wszonja@gmail.com>
2023-03-13 17:42:39 +0100

    add contents from cookiecutter template

---
+++ src/pinto_lab_to_nwb/into_the_void/into_the_voidnwbconverter.py
@@ -0,0 +1,20 @@
+"""Primary NWBConverter class for this dataset."""
+from neuroconv import NWBConverter
+from neuroconv.datainterfaces import (
+    SpikeGLXRecordingInterface,
+    SpikeGLXLFPInterface,
+    PhySortingInterface,
+)
+
+from pinto_lab_to_nwb.into_the_void import IntoTheVoidBehaviorInterface
+
+
+class IntoTheVoidNWBConverter(NWBConverter):
+    """Primary conversion class for my extracellular electrophysiology dataset."""
+
+    data_interface_classes = dict(
+        Recording=SpikeGLXRecordingInterface,
+        LFP=SpikeGLXLFPInterface,
+        Sorting=PhySortingInterface,
+        Behavior=IntoTheVoidBehaviorInterface,
+    )
