da6bbbad8dd560cae6f4726e46b0069dcd4de92a
weiglszonja <wszonja@gmail.com>
2023-08-08 20:23:52 +0200

    add skeleton for conversion of bruker imaging

--- src/pinto_lab_to_nwb/into_the_void/into_the_void_convert_session.py
+++ src/pinto_lab_to_nwb/into_the_void/into_the_void_convert_session.py
@@ -1,68 +1,92 @@
 """Primary script to run to convert an entire session for of data using the NWBConverter."""
+import re
 from pathlib import Path
-from typing import Union
-import datetime
-from zoneinfo import ZoneInfo
-
-from neuroconv.utils import load_dict_from_file, dict_deep_update
+from typing import Optional
+from dateutil import tz
+from neuroconv.utils import (
+    load_dict_from_file,
+    dict_deep_update,
+    FolderPathType,
+    FilePathType,
+)
 
 from pinto_lab_to_nwb.into_the_void import IntoTheVoidNWBConverter
 
 
-def session_to_nwb(data_dir_path: Union[str, Path], output_dir_path: Union[str, Path], stub_test: bool = False):
-    data_dir_path = Path(data_dir_path)
-    output_dir_path = Path(output_dir_path)
-    if stub_test:
-        output_dir_path = output_dir_path / "nwb_stub"
-    output_dir_path.mkdir(parents=True, exist_ok=True)
+def session_to_nwb(
+    nwbfile_path: FilePathType,
+    two_photon_imaging_folder_path: FolderPathType,
+    segmentation_folder_path: Optional[FolderPathType] = None,
+    stub_test: bool = False,
+):
+    """
+    Converts a single session to NWB.
 
-    session_id = "subject_identifier_usually"
-    nwbfile_path = output_dir_path / f"{session_id}.nwb"
+    Parameters
+    ----------
+    nwbfile_path : FilePathType
+        The file path to the NWB file that will be created.
+    two_photon_imaging_folder_path: FolderPathType
+        The folder path that contains the Bruker TIF imaging output (.ome.tif files).
+    segmentation_folder_path: FolderPathType
+        The folder that contains the Suite2P segmentation output.
+    stub_test: bool, optional
+        For testing purposes, when stub_test=True only writes a subset of imaging and segmentation data.
+    """
 
     source_data = dict()
     conversion_options = dict()
 
-    # Add Recording
-    source_data.update(dict(Recording=dict()))
-    conversion_options.update(dict(Recording=dict()))
+    # Add Imaging
+    imaging_source_data = dict(folder_path=str(two_photon_imaging_folder_path))
+    source_data.update(dict(Imaging=imaging_source_data))
+    conversion_options.update(dict(Imaging=dict(stub_test=stub_test)))
 
-    # Add LFP
-    source_data.update(dict(LFP=dict()))
-    conversion_options.update(dict(LFP=dict()))
-
-    # Add Sorting
-    source_data.update(dict(Sorting=dict()))
-    conversion_options.update(dict(Sorting=dict()))
-
-    # Add Behavior
-    source_data.update(dict(Behavior=dict()))
-    conversion_options.update(dict(Behavior=dict()))
+    # Add Segmentation (optional)
+    if segmentation_folder_path:
+        segmentation_source_data = dict(folder_path=str(segmentation_folder_path))
+        source_data.update(dict(Segmentation=segmentation_source_data))
+        conversion_options.update(dict(Segmentation=dict(stub_test=False)))
 
     converter = IntoTheVoidNWBConverter(source_data=source_data)
 
     # Add datetime to conversion
     metadata = converter.get_metadata()
-    datetime.datetime(year=2020, month=1, day=1, tzinfo=ZoneInfo("US/Eastern"))
-    date = datetime.datetime.today()  # TO-DO: Get this from author
-    metadata["NWBFile"]["session_start_time"] = date
+    # For data provenance we can add the time zone information to the conversion if missing
+    session_start_time = metadata["NWBFile"]["session_start_time"]
+    tzinfo = tz.gettz("US/Pacific")
+    metadata["NWBFile"].update(session_start_time=session_start_time.replace(tzinfo=tzinfo))
 
     # Update default metadata with the editable in the corresponding yaml file
-    editable_metadata_path = Path(__file__).parent / "into_the_void_metadata.yaml"
+    editable_metadata_path = Path(__file__).parent / "general_metadata.yaml"
     editable_metadata = load_dict_from_file(editable_metadata_path)
     metadata = dict_deep_update(metadata, editable_metadata)
 
+    # Update metadata with subject_id and session_id from folder_path
+    # NCCR51_2023_04_07_no_task_dual_color_jrgeco_t_series-001
+    file_naming_pattern = r"^(?P<subject_id>[^_]+)_(?:\d{4}_\d{2}_\d{2}_)(?P<session_id>.+)"
+    match = re.match(file_naming_pattern, str(two_photon_imaging_folder_path))
+    if match:
+        groups_dict = match.groupdict()
+        metadata["NWBFile"].update(session_id=groups_dict["session_id"].replace("_", "-"))
+        metadata["Subject"].update(subject_id=groups_dict["subject_id"])
+
     # Run conversion
-    converter.run_conversion(metadata=metadata, nwbfile_path=nwbfile_path, conversion_options=conversion_options)
+    converter.run_conversion(
+        nwbfile_path=nwbfile_path, metadata=metadata, overwrite=True, conversion_options=conversion_options
+    )
 
 
 if __name__ == "__main__":
     # Parameters for conversion
-    data_dir_path = Path("/Directory/With/Raw/Formats/")
-    output_dir_path = Path("~/conversion_nwb/")
-    stub_test = False
+    imaging_folder_path = Path("/Volumes/t7-ssd/Pinto/NCCR62_2023_07_06_IntoTheVoid_t_series_Dual_color-000")
+    segmentation_folder_path = imaging_folder_path / "suite2p"
+    nwbfile_path = Path("/Volumes/t7-ssd/Pinto/nwbfiles/imaging_stub2.nwb")
+    stub_test = True
 
     session_to_nwb(
-        data_dir_path=data_dir_path,
-        output_dir_path=output_dir_path,
+        nwbfile_path=nwbfile_path,
+        two_photon_imaging_folder_path=imaging_folder_path,
+        segmentation_folder_path=segmentation_folder_path,
         stub_test=stub_test,
     )
--- src/pinto_lab_to_nwb/into_the_void/into_the_voidnwbconverter.py
+++ src/pinto_lab_to_nwb/into_the_void/into_the_voidnwbconverter.py
@@ -1,20 +1,13 @@
 """Primary NWBConverter class for this dataset."""
 from neuroconv import NWBConverter
-from neuroconv.datainterfaces import (
-    SpikeGLXRecordingInterface,
-    SpikeGLXLFPInterface,
-    PhySortingInterface,
-)
-
-from pinto_lab_to_nwb.into_the_void import IntoTheVoidBehaviorInterface
+from neuroconv.datainterfaces import Suite2pSegmentationInterface
+from neuroconv.converters import BrukerTiffSinglePlaneConverter
 
 
 class IntoTheVoidNWBConverter(NWBConverter):
     """Primary conversion class for my extracellular electrophysiology dataset."""
 
     data_interface_classes = dict(
-        Recording=SpikeGLXRecordingInterface,
-        LFP=SpikeGLXLFPInterface,
-        Sorting=PhySortingInterface,
-        Behavior=IntoTheVoidBehaviorInterface,
+        Imaging=BrukerTiffSinglePlaneConverter,
+        Segmentation=Suite2pSegmentationInterface,
     )
