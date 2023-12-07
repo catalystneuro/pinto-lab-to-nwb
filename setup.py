e1557a31268ed535956995352cc2940469b226c1
weiglszonja <wszonja@gmail.com>
2023-12-07 21:44:48 +0100

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pinto-lab-to-nwb",
-    version="0.0.1",
+    version="0.1.0",
     description="NWB conversion scripts, functions, and classes for Pinto conversion",
     long_description=long_description,
     long_description_content_type="text/markdown",
